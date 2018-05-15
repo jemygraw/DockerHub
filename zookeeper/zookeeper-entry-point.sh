12982b07957679d6be62ded75c76d5482db382f1
Jemy <jemygraw@gmail.com>
2018-05-15 09:14:17 +0800

    fix to solve the listen host in docker for zookeeper, use 0.0.0.0 to bind to ZOOKEEPER_HOST env

--- zookeeper/zookeeper-entry-point.sh
+++ zookeeper/zookeeper-entry-point.sh
@@ -1,7 +1,15 @@
 if [ -z "$ZOOKEEPER_MYID" ]; then
-echo "error: zookeeper need a myid file under the dataDir path\n Your need to specify ZOOKEEPER_MYID before starting the service"
+	echo "error: zookeeper need a myid file under the dataDir path\n Your need to specify ZOOKEEPER_MYID before starting the service"
 exit 1
 fi 
 
+if [ -z "$ZOOKEEPER_HOST" ]; then
+	echo "error: You need to specify a ZOOKEEPER_HOST to bind to 0.0.0.0 in the /etc/hosts"
+exit 1
+fi
+
+
 echo $ZOOKEEPER_MYID >> /root/zookeeper/data/myid
+echo "0.0.0.0 $ZOOKEEPER_HOST" >> /etc/hosts
+
 /root/zookeeper/bin/zkServer.sh start-foreground
